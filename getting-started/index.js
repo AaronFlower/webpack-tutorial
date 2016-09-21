959c4c03416fca9f6a6849ab9b783d04a9161d07
AaronFlower <haojunzhan@gmail.com>
2016-09-21 18:59:49 +0800

    Using require method to import include another module.

--- getting-started/index.js
+++ getting-started/index.js
@@ -1,1 +1,9 @@
 document.write('Hello webpack, it works!')
+document.write(require('./content.js'))
+
+/**
+ * Webpack will analyze your entry file for dependencies to other files.
+ * This files(called modules) are include in your bundle.js too.
+ * Web will give each module a unique id and save all modules accessible by id in the bundle.js file.
+ * Only the entry module is executed on startup. A small runtime provides the require function and executes the dependencies when requried.
+ */
