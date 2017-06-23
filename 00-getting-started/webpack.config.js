0b66d5a9f9b55875c3bf2fee43a6a208a1879986
AaronFlower <haojunzhan@gmail.com>
2017-06-23 22:15:37 +0800

    add html-webpack-plugin

--- 00-getting-started/webpack.config.js
+++ 00-getting-started/webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path')
 const distPath = path.resolve(__dirname, 'dist')
+const htmlPlugin = require('html-webpack-plugin')
 
 let config = {
 	'entry': './src/index.js',
 	'output': {
 		'path': distPath,
 		'filename': 'bundle.js',
-	}
+	},
+
+	'plugins': [
+		new htmlPlugin({
+			title: 'Webpack getting started'
+		})
+	]
 }
 
 module.exports = config
