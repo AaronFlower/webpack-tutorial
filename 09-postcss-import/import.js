e7e26ae79dad315eea118861f7d64c1357778b12
AaronFlower <haojunzhan@gmail.com>
2017-11-20 10:19:25 +0800

    postcss-import test

---
+++ 09-postcss-import/import.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+var postcss = require('postcss')
+var atImport = require('postcss-import')
+
+
+// css to be processed
+var css = fs.readFileSync('input.css', 'utf8')
+
+// process css
+postcss()
+	.use(atImport())
+	.process(css, {
+		from: 'input.css'
+	})
+	.then((result) => {
+		var output = result.css
+		console.log(output)
+	})
