c54ba864f5c6020dc066c10327266293caf22454
AaronFlower <haojunzhan@gmail.com>
2017-11-20 13:32:30 +0800

    use another postcss.config.js

--- 09-postcss-import/postcss.config.js
+++ 09-postcss-import/postcss.config.js
@@ -1,5 +1,17 @@
+/* 
+// 可以自己指定位置。
 const atImport = require('postcss-import')
 console.log('with: postcss.config.js')
 module.exports = {
 	plugins: [atImport]
+}
+*/
+
+/**
+ * 也可以让 Postcss 帮你引入。
+ */
+module.exports = {
+	plugins: {
+		'postcss-import': {}
+	}
 }
